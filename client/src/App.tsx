2721e8c6dc58dd7d7eeb1eb6fae3fa465be87a54
Ayush Mamgain <ayushmamgain349@gmail.com>
2025-12-25 07:11:23 +0530

    feat: integrate Tailwind CSS, Axios, and dotenv; update App component styling
    - Added Tailwind CSS and its Vite plugin to the project.
    - Included Axios for HTTP requests and dotenv for environment variable management.
    - Updated the App component to use Tailwind CSS for styling.
    - Replaced existing CSS with Tailwind's utility classes.
    - Created a new Axios instance for API calls with a base URL from environment variables.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen w-screen bg-zinc-900 text-white'>
+      <div className='text-2xl text-center'>App</div>
+    </div>
   );
 };
